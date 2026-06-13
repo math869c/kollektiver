eaa785412df5d4bfe0048a679dacb400af655185
Mathias Berg <mathias.held.berg@gmail.com>
2026-06-13 16:16:31 +0200

    mango

---
+++ kollektivet/MANGO LASSIE/config.js
@@ -0,0 +1,14 @@
+window.KOLLEKTIV_CONFIG = {
+    "slug": "MANGO LASSIE",
+    "navn": "MANGO LASSIE",
+    "tekst": "Jeg glæder mig til at dele en hverdag med jer. Fællesspisning, hænge ud i stuen efter en lang dag, tage en kold øl i solen eller ende i et brætspil, der varer alt for længe. Det lyder som om, I har skabt et virkelig stærkt fællesskab omkring Mango Lassie, og jeg ville elske at være en del af",
+    "kommentarer": {
+        "basal": "",
+        "faellesskab": "",
+        "renlig": "",
+        "mad": "",
+        "noerd": "",
+        "beboer": "",
+        "jer": ""
+    }
+};
